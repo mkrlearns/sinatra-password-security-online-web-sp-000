c67777df587aac0a86915cc5eb5ca44e24c5974a
ryan m <contact@ryanmeek.com>
2020-02-29 23:59:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,37 @@ class ApplicationController < Sinatra::Base
 		set :session_secret, "password_security"
 	end
 
-	get("/") {erb :index}
-	get("/signup") {erb :signup}
-	get("/login") {erb :login}
+	get "/" do
+		erb :index
+	end
+
+	get "/signup" do
+		erb :signup
+	end
+
+	post "/signup" do
+		user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
+	end
+
+	get "/login" do
+		erb :login
+	end
+
+	post "/login" do
+		user = User.find_by(:username => params[:username])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		else
+			redirect "failure"
+		end
+	end
+
 	get "/success" do
 		if logged_in?
 			erb :success
@@ -18,41 +46,24 @@ class ApplicationController < Sinatra::Base
 			redirect "/login"
 		end
 	end
-	get("/failure") {erb :failure}
-	get("/logout") {session.clear; redirect "/"}
 
-	post "/signup" do
-	  user = User.new(:username => params[:username], :password => params[:password])
-
-	  if user.save
-	    redirect "/login"
-	  else
-	    redirect "/failure"
-	  end
+	get "/failure" do
+		erb :failure
 	end
 
-	post "/login" do
-	  user = User.find_by(:username => params[:username])
-
-	  if user && user.authenticate(params[:password])
-	    session[:user_id] = user.id
-	    redirect "/success"
-	  else
-	    redirect "/failure"
-	  end
+	get "/logout" do
+		session.clear
+		redirect "/"
 	end
-
-	# get "/success" do
-	# 	if logged_in?
-	# 		erb :success
-	# 	else
-	# 		redirect "/login"
-	# 	end
-	# end
 
 	helpers do
-		def logged_in?() !!session[:id] end
-		def current_user() User.find(session[:id]) end
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find(session[:user_id])
+		end
 	end
 
 end
