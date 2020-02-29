83dc281ced00ab2513bc8ebfde291f0b27527aa1
ryan m <contact@ryanmeek.com>
2020-02-29 23:41:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,51 +8,33 @@ class ApplicationController < Sinatra::Base
 		set :session_secret, "password_security"
 	end
 
-	get "/" do
-		erb :index
-	end
+	get("/") {erb :index}
+	get("/signup") {erb :signup}
+	get("/login") {erb :login}
+	get("/success") {logged_in? ? erb :success : redirect "/login"}
+	get("/failure") {erb :failure}
+	get("/logout") {session.clear; redirect "/"}
 
-	get "/signup" do
-		erb :signup
-	end
+	post("/signup") {user = User.new(:username => params[:username], :password => params[:password])}
 
-	post "/signup" do
-		#your code here!
-	end
 
-	get "/login" do
-		erb :login
-	end
+
 
 	post "/login" do
 		#your code here!
 	end
 
-	get "/success" do
-		if logged_in?
-			erb :success
-		else
-			redirect "/login"
-		end
-	end
-
-	get "/failure" do
-		erb :failure
-	end
-
-	get "/logout" do
-		session.clear
-		redirect "/"
-	end
+	# get "/success" do
+	# 	if logged_in?
+	# 		erb :success
+	# 	else
+	# 		redirect "/login"
+	# 	end
+	# end
 
 	helpers do
-		def logged_in?
-			!!session[:id]
-		end
-
-		def current_user
-			User.find(session[:id])
-		end
+		def logged_in?() !!session[:id] end
+		def current_user() User.find(session[:id]) end
 	end
 
 end
