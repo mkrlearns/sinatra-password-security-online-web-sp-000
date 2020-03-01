dd3b9f5b7ef9be55fb17c0e446a8b0908d216f9d
ryan m <contact@ryanmeek.com>
2020-03-01 00:00:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -58,10 +58,7 @@ class ApplicationController < Sinatra::Base
 
 	helpers do
 		def logged_in?() !!session[:user_id] end
-
-		def current_user
-			User.find(session[:user_id])
-		end
+		def current_user() User.find(session[:user_id]) end
 	end
 
 end
